91449fecd2cf65cbc39bb7fc64b7b04917160412
yunna <homer_j@hanmail.net>
2024-01-20 01:19:35 +0900

    feat : 홈 화면 구현

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,41 @@
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+
+import LogoIcon from 'assets/imageComponents/LogoIcon';
+import { PATHS } from 'constants/paths';
+
 const Home = () => {
-  return <h1>home 페이지</h1>;
+  const navigate = useNavigate();
+  return (
+    <Wrapper>
+      <LogoIcon />
+      <Button onClick={() => navigate(PATHS.ORDER)}>주문하러 가기</Button>
+    </Wrapper>
+  );
 };
 
 export default Home;
+
+const Wrapper = styled.div`
+  background-color: #000;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 41px;
+`;
+
+const Button = styled.button`
+  background-color: #fff;
+  width: 172px;
+  height: 88px;
+  border-radius: 20px;
+  font-size: 25px;
+  transition: background-color 0.1s, color 0.1s;
+
+  &:hover {
+    background-color: #f75a2f;
+    color: #fff;
+  }
+`;
